be898cfe0574a7c842a8c946682b9e6e803bcfa8
sotudeko <sotudeko@sonatype.com>
2019-03-21 11:19:11 +0000

    0.13

--- bin/nxrm3-component-upload.sh
+++ bin/nxrm3-component-upload.sh
@@ -7,7 +7,7 @@ version=$4
 artifact_path=/Users/sotudeko/Development/scan-artifacts/java/webgoat-server-8.0.0.M21.jar
 extension=jar
 
-playbook=component-upload
+playbook=nxrm3-component-upload
 
 ansible-playbook -v ../playbooks/${playbook}.yml --extra-vars "repository=${repository} group=${group} artifact=${artifact} version=${version} artifact_path=${artifact_path} extension=${extension}"
 
