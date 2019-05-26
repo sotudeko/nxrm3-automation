7cf05f315d3005bd980febd88a37b88fc2abfd6e
sotudeko <sotudeko@sonatype.com>
2019-05-26 18:36:09 +0100

    0.34

--- listener/listener.js
+++ listener/listener.js
@@ -15,7 +15,26 @@ app.use(bodyParser.urlencoded({
 const server = app.listen(3007, () => console.log('[nxrm3-listener] listening on port 3007'));
 
 app.post('/', (req, res) => {
-    console.log(req.body);
+    const payload = req.body
+    console.log(payload);
+
+    if (payload.action === 'CREATED'){
+      if (payload.component.format === 'docker'){
+        console.log('docker pull sola.local:18087/' + payload.component.name + ':' + payload.component.version)
+      }
+      if (payload.component.format === 'maven2'){
+        console.log('  <dependency>')
+        console.log('    <groupId>' + payload.component.group + '</group>')
+        console.log('    <artifactId>' + payload.component.name + '</artifactId>')
+        console.log('    <version>' + payload.component.version + '</version>')
+        console.log('  </dependency>')
+      }
+    }
+
+    if (payload.action === 'DELETED'){
+      console.log('curl -s -u admin:admin123 -X DELETE http://localhost:8082/service/rest/v1/components/' + payload.component.id)
+    }
+
 
     // const ls = spawn('ls', ['-lh', '/usr']);
 
